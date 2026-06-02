1e6e537d2d625183024775acb2e11010701fac0d
sergionietolabian <sergionietolabian@gmail.com>
2026-06-02 10:53:39 +0200

    Creados los creates, put y deletes
    Creados los create, put y delete y sus endpoints
    /tasks
    /tasks/{id} put
    /tasks/{id} delete

--- src/main/java/com/sergionietolabian/springbootapi/controller/TaskController.java
+++ src/main/java/com/sergionietolabian/springbootapi/controller/TaskController.java
@@ -2,8 +2,14 @@ package com.sergionietolabian.springbootapi.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +46,28 @@ public class TaskController {
             @RequestParam TaskStatus status,
             @RequestParam String title) {
         return taskService.getTasksByStatusAndTitle(status, title);
+    }
+    
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED)
+        		.body(createdTask);
+    }
+    
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(
+            @PathVariable Long id,
+            @RequestBody Task task) {
+
+        return taskService.updateTask(id, task);
+    }
+    
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+
+        taskService.deleteTask(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/sergionietolabian/springbootapi/service/TaskService.java
+++ src/main/java/com/sergionietolabian/springbootapi/service/TaskService.java
@@ -33,4 +33,36 @@ public class TaskService {
     public List<Task> getTasksByStatusAndTitle(TaskStatus status, String title) {
         return taskRepository.findByStatusAndTitleContaining(status, title);
     }
+    
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+    
+    public Task updateTask(Long id, Task updatedTask) {
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found with id " + id));
+
+        if (updatedTask.getTitle() != null) {
+            task.setTitle(updatedTask.getTitle());
+        }
+
+        if (updatedTask.getDescription() != null) {
+            task.setDescription(updatedTask.getDescription());
+        }
+
+        if (updatedTask.getStatus() != null) {
+            task.setStatus(updatedTask.getStatus());
+        }
+
+        return taskRepository.save(task);
+    }
+    
+    public void deleteTask(Long id) {
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found with id " + id));
+
+        taskRepository.delete(task);
+    }
 }
